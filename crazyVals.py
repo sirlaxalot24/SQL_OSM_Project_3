6eb7bad21d86faf4e0509c0951ffe09586616d83
sirlaxalot24 <j.a.paukovits@gmail.com>
2016-08-28 12:44:18 -0500

    First Commit

---
+++ crazyVals.py
@@ -0,0 +1,44 @@
+import csv
+from pprint import pprint as pp
+import cleanStreeZip
+
+
+# this file is used to check the CVS's for systemic issues with the data
+
+
+expected = ["Street", "Avenue", "Boulevard", "Drive", "Court", "Place", "Square", "Lane", "Road",
+            "Trail", "Parkway", "Commons", "Terrace", "Way", "Highway", "Plaza", "Circle"]
+
+topVar = 'type'
+bVar = 'key'
+param = 'street'
+fileName = 'nodes_tags.csv'
+
+
+crazyVals = {}
+
+with open(fileName, 'r') as f:
+    tags = csv.DictReader(f)
+    for row in tags:
+
+        # run this statement when looking for odd values
+
+        if row[topVar] == param and bVar == 'value':
+            # zipCode = cleanStreeZip.clean_street_name(row[bVar])
+            streetName = row[bVar]
+            # print streetName
+            if streetName in expected:
+                pass
+            elif streetName not in crazyVals:
+                crazyVals[streetName] = 1
+            else:
+                crazyVals[streetName] += 1
+
+            # run this statement when exploring keys and types
+        else:
+            if row[bVar] not in crazyVals:
+                crazyVals[row[bVar]] = 1
+            else:
+                crazyVals[row[bVar]] += 1
+
+pp(crazyVals)
